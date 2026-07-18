6bee897648e00fc46c8183444b0fc0de29018d07
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-18 15:56:30 +0300

    admin_contact_infos

---
+++ Portfolio/Controllers/ContactInfoController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Internal;
+using Portfolio.Data.Context;
+using Portfolio.Data.Entities;
+
+namespace Portfolio.Controllers
+{
+    public class ContactInfoController : BaseAdminController
+    {
+        private readonly AppDBContext _context;
+
+        public ContactInfoController(AppDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            var contactinfo = _context.ContactInfos.FirstOrDefault();
+            return View(contactinfo);
+        }
+        [HttpGet("CreateContactInfo")]
+        public IActionResult CreateContactInfo()
+        {
+            return View();
+        }
+        [HttpPost("CreateContactInfo")]
+        public IActionResult CreateContactInfo(ContactInfo contactInfo)
+        {
+            _context.ContactInfos.Add(contactInfo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet("UpdateContactInfo/{id}")]
+        public IActionResult UpdateContactInfo(int id)
+        {
+            var contactInfo = _context.ContactInfos.Find(id);
+            return View(contactInfo);
+        }
+        [HttpPost("UpdateContactInfo")]
+        public IActionResult UpdateContactInfo(ContactInfo contactInfo)
+        {
+            _context.ContactInfos.Update(contactInfo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet("DeleteContactInfo/{id}")]
+        public IActionResult DeleteContactInfo(int id)
+        {
+            var contactInfo = _context.ContactInfos.Find(id);
+            _context.ContactInfos.Remove(contactInfo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
