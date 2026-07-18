663d17760912dd9292334ed1077465ed7984755d
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-18 16:18:02 +0300

    admin_userMessages_CRUD

--- Portfolio/Controllers/MessageController.cs
+++ Portfolio/Controllers/MessageController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Internal;
+using Portfolio.Data.Context;
+using Portfolio.Data.Entities;
 
 namespace Portfolio.Controllers
 {
-    public class MessageController : Controller
+    public class MessageController : BaseAdminController
     {
+        private readonly AppDBContext _context;
+
+        public MessageController(AppDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
         public IActionResult Index()
         {
-            return View();
+            var messages = _context.UserMessages.ToList();
+            return View(messages);
+        }
+        [HttpPost("ReadMessageApi/{id}")]
+        public IActionResult ReadMessageApi(int id)
+        {
+            var message = _context.UserMessages.Find(id);
+            if (message != null)
+            {
+                message.Isread = true;
+                _context.SaveChanges();
+                return Ok(new { success = true });
+            }
+            return NotFound();
+        }
+
+
+        [HttpGet("DeleteMessage/{id}")]
+        public IActionResult DeleteMessage(int id)
+        {
+            var message = _context.UserMessages.Find(id);
+            _context.UserMessages.Remove(message);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
+
