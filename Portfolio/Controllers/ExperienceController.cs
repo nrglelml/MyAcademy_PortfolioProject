78f4493cdf3270368a9e99dfc824852cc79e4476
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-18 15:31:35 +0300

    admin_experience_CRUD

---
+++ Portfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Internal;
+using Portfolio.Data.Context;
+using Portfolio.Data.Entities;
+
+namespace Portfolio.Controllers
+{
+    public class ExperienceController : BaseAdminController
+    {
+        private readonly AppDBContext _context;
+
+        public ExperienceController(AppDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            var experience = _context.Experiences.ToList();
+            return View(experience);
+        }
+        [HttpGet("CreateExperience")]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+        [HttpPost("CreateExperience")]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            _context.Experiences.Add(experience);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet("UpdateExperience/{id}")]
+        public IActionResult UpdateExperience(int id)
+        {
+            var experience = _context.Experiences.Find(id);
+            return View(experience);
+        }
+        [HttpPost("UpdateExperience")]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            _context.Experiences.Update(experience);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet("DeleteExperience/{id}")]
+        public IActionResult DeleteExperience(int id)
+        {
+            var experience = _context.Experiences.Find(id);
+            _context.Experiences.Remove(experience);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
