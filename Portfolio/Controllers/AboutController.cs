34c3f5161a587bb411a073b5aa573e6a84992c46
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-22 11:53:39 +0300

    saveImageFile_function for save images to project

--- Portfolio/Controllers/AboutController.cs
+++ Portfolio/Controllers/AboutController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Portfolio.Data.Context;
 using Portfolio.Data.Entities;
 
@@ -17,38 +18,109 @@ namespace Portfolio.Controllers
             var about = _context.Abouts.FirstOrDefault();
             return View(about);
         }
+
         [HttpGet("CreateAbout")]
         public IActionResult CreateAbout()
         {
             return View();
         }
+
         [HttpPost("CreateAbout")]
-        public IActionResult CreateAbout(About about)
+        public IActionResult CreateAbout(About about, IFormFile? imageFile)
         {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                about.ImageUrl = SaveImageFile(imageFile);
+            }
+
             _context.Abouts.Add(about);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
         [HttpGet("UpdateAbout/{id}")]
         public IActionResult UpdateAbout(int id)
         {
-            var about=_context.Abouts.Find(id);
+            var about = _context.Abouts.Find(id);
+            if (about == null)
+            {
+                return NotFound();
+            }
             return View(about);
         }
+
         [HttpPost("UpdateAbout")]
-        public IActionResult UpdateAbout(About about)
+        public IActionResult UpdateAbout(About about, IFormFile? imageFile)
         {
+            var existingAbout = _context.Abouts.AsNoTracking().FirstOrDefault(x => x.Id == about.Id);
+
+            if (existingAbout != null)
+            {
+                if (imageFile != null && imageFile.Length > 0)
+                {
+                    if (!string.IsNullOrEmpty(existingAbout.ImageUrl))
+                    {
+                        var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", existingAbout.ImageUrl.TrimStart('/'));
+                        if (System.IO.File.Exists(oldPath))
+                        {
+                            System.IO.File.Delete(oldPath);
+                        }
+                    }
+
+                    about.ImageUrl = SaveImageFile(imageFile);
+                }
+                else
+                {
+                    about.ImageUrl = existingAbout.ImageUrl;
+                }
+            }
+
             _context.Abouts.Update(about);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
         [HttpGet("DeleteAbout/{id}")]
         public IActionResult DeleteAbout(int id)
         {
-            var about=_context.Abouts.Find(id);
-            _context.Abouts.Remove(about);
-            _context.SaveChanges();
+            var about = _context.Abouts.Find(id);
+            if (about != null)
+            {
+                if (!string.IsNullOrEmpty(about.ImageUrl))
+                {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", about.ImageUrl.TrimStart('/'));
+                    if (System.IO.File.Exists(path))
+                    {
+                        System.IO.File.Delete(path);
+                    }
+                }
+
+                _context.Abouts.Remove(about);
+                _context.SaveChanges();
+            }
+
             return RedirectToAction("Index");
+        }
+
+        private string SaveImageFile(IFormFile imageFile)
+        {
+            var extension = Path.GetExtension(imageFile.FileName);
+            var imagename = Guid.NewGuid() + extension;
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "aboutImages");
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            var savelocation = Path.Combine(folder, imagename);
+
+            using (var stream = new FileStream(savelocation, FileMode.Create))
+            {
+                imageFile.CopyTo(stream);
+            }
+
+            return "/aboutImages/" + imagename;
         }
     }
 }
--- Portfolio/Controllers/ProjectController.cs
+++ Portfolio/Controllers/ProjectController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Portfolio.Data.Context;
 using Portfolio.Data.Entities;
 using System;
+using System.IO;
+using System.Linq;
 
 namespace Portfolio.Controllers
 {
@@ -13,6 +17,7 @@ namespace Portfolio.Controllers
         {
             _context = context;
         }
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -27,12 +32,20 @@ namespace Portfolio.Controllers
         }
 
         [HttpPost("CreateProject")]
-        public IActionResult CreateProject(Project project)
+        public IActionResult CreateProject(Project project, IFormFile? imageFile)
         {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                project.ImageUrl = SaveImageFile(imageFile);
+            }
+
+            ModelState.Remove("ImageUrl");
+
             if (!ModelState.IsValid)
             {
                 return View(project);
             }
+
             _context.Projects.Add(project);
             _context.SaveChanges();
             return RedirectToAction("Index");
@@ -42,28 +55,92 @@ namespace Portfolio.Controllers
         public IActionResult UpdateProject(int id)
         {
             var project = _context.Projects.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
             return View(project);
         }
 
         [HttpPost("UpdateProject")]
-        public IActionResult UpdateProject(Project project)
+        public IActionResult UpdateProject(Project project, IFormFile? imageFile)
         {
+            var existingProject = _context.Projects.AsNoTracking().FirstOrDefault(x => x.Id == project.Id);
+
+            if (existingProject != null)
+            {
+                if (imageFile != null && imageFile.Length > 0)
+                {
+                    if (!string.IsNullOrEmpty(existingProject.ImageUrl))
+                    {
+                        var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", existingProject.ImageUrl.TrimStart('/'));
+                        if (System.IO.File.Exists(oldPath))
+                        {
+                            System.IO.File.Delete(oldPath);
+                        }
+                    }
+
+                    project.ImageUrl = SaveImageFile(imageFile);
+                }
+                else
+                {
+                    project.ImageUrl = existingProject.ImageUrl;
+                }
+            }
+
+            ModelState.Remove("ImageUrl");
+
             if (!ModelState.IsValid)
             {
                 return View(project);
             }
+
             _context.Projects.Update(project);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
-        
+
         [HttpGet("DeleteProject/{id}")]
         public IActionResult DeleteProject(int id)
         {
             var project = _context.Projects.Find(id);
-            _context.Projects.Remove(project);
-            _context.SaveChanges();
+            if (project != null)
+            {
+                if (!string.IsNullOrEmpty(project.ImageUrl))
+                {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", project.ImageUrl.TrimStart('/'));
+                    if (System.IO.File.Exists(path))
+                    {
+                        System.IO.File.Delete(path);
+                    }
+                }
+
+                _context.Projects.Remove(project);
+                _context.SaveChanges();
+            }
+
             return RedirectToAction("Index");
+        }
+
+        private string SaveImageFile(IFormFile imageFile)
+        {
+            var extension = Path.GetExtension(imageFile.FileName);
+            var imagename = Guid.NewGuid() + extension;
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "projectImages");
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            var savelocation = Path.Combine(folder, imagename);
+
+            using (var stream = new FileStream(savelocation, FileMode.Create))
+            {
+                imageFile.CopyTo(stream);
+            }
+
+            return "/projectImages/" + imagename;
         }
     }
 }
