c7511ffc416c3d213b563e898b0bc9666f4f1c0c
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-07-23 11:54:35 +0300

    error_page

---
+++ Portfolio/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(int code)
+        {
+            return View();
+        }
+    }
+}
